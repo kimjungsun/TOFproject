b8976f1552ef682283dc8771671e73cf12b518b1
Megan Kim <33720329+kimjungsun@users.noreply.github.com>
2020-05-19 18:18:39 +0900

    Update subserver.c

--- subserver.c
+++ subserver.c
@@ -1,19 +1,3 @@
-#include "sub_server.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include<pthread.h>
-// 전역 변수 선언 ?
-int gStartAmpDistVideo;
-char buffer2 = '0';
-
-void *startSubserver(void *arg)
-{
-        int sock;
-        struct sockaddr_in serv_addr;
 
         int status ;
         pthread_t tid[5];
@@ -25,8 +9,8 @@ void *startSubserver(void *arg)
 
         memset(&serv_addr, 0, sizeof(serv_addr));
         serv_addr.sin_family = AF_INET;
-        serv_addr.sin_addr.s_addr = inet_addr("192.168.1.129");
-        serv_addr.sin_port = htons(7777);
+        serv_addr.sin_addr.s_addr = inet_addr("192.168.7.33");
+        serv_addr.sin_port = htons(7811);
 
         if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) == -1)
                error_handling("connect() error");
@@ -41,14 +25,17 @@ void *startSubserver(void *arg)
        			}
 
         pthread_join(tid[0],(void **)&state);
+        printf("쓰레드 write 종료\n");
 
         pthread_join(tid[1],(void **)&state);
+        printf("쓰레드 read 종료\n");
 
         close(sock);
                 return 0;
         }
 
 void *thr_write(void *arg){
+	printf("thr_write initialized\n");
 	int sock = (int)*((int *)arg);
     char send_meg = '0';
 	while(1){
@@ -63,11 +50,15 @@ void *thr_write(void *arg){
 void *thr_read(void *arg){
     char recv ;
 	int sock = (int)*((int *)arg);
+	printf("thr_read initialized\n");
+
 	while(1){
 	if(read(sock,&recv, sizeof(recv))!=0)
             printf("Recieve from server:%c\n" ,recv);
 	buffer2 = recv ;
+	printf("BUFFER : %c\n",buffer2);
 	}
+	printf(" 쓰레드 파괴됨...\n");
 
 }
 
