6c731b0f42f6c1a45fbd72ca3d9f21b6ef308cb9
Megan Kim <33720329+kimjungsun@users.noreply.github.com>
2020-05-19 13:42:30 +0900

    Add files via upload

--- server.c
+++ server.c
@@ -10,19 +10,23 @@
 
 #include <sys/socket.h>
 
+#include<pthread.h>
+
 void error_handling(char *message);
+void *thr_func(void *arg);
+int clnt_sock1,clnt_sock2;
+int clientsock[2] ;
+char buffer;
 
 int main(int argc, char *argv[])
 
 {
-        int str_len;
-	int sock;
-        int clnt_sock;
-	struct sockaddr_in serv_addr;
+        pthread_t tid[5];
+	int status;
+	int sock1,sock2;
+	struct sockaddr_in serv_addr1,serv_addr2;
 	struct sockaddr_in clnt_addr;
         int clnt_addr_size = sizeof(clnt_addr);
-	char message[10];
-        char s_message[6]="zyxqub";
 	int str_len;
 
 	if (argc != 2)
@@ -30,38 +34,64 @@ int main(int argc, char *argv[])
 		printf("Usage: %s <IP> <port>\n", argv[0]);
 		exit(1);
 	}
-	sock = socket(PF_INET, SOCK_STREAM, 0); //소켓을 생성하고 있다.
-	if (sock == -1)
+	sock1 = socket(PF_INET, SOCK_STREAM, 0); //소켓을 생성하고 있다.
+	sock2 = socket(PF_INET, SOCK_STREAM, 0); //소켓을 생성하고 있다.
+	if (sock1== -1)
+		error_handling("socket() error");
+	if (sock2== -1)
 		error_handling("socket() error");
 
-	memset(&serv_addr, 0, sizeof(serv_addr));
+	memset(&serv_addr1, 0, sizeof(serv_addr1));
+	memset(&serv_addr2, 0, sizeof(serv_addr2));
 
-	serv_addr.sin_family = AF_INET;
+	serv_addr1.sin_family = AF_INET;
+	serv_addr1.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr1.sin_port = htons(atoi(argv[1]));
+	serv_addr2.sin_family = AF_INET;
+	serv_addr2.sin_addr.s_addr = htonl(INADDR_ANY);
+	serv_addr2.sin_port = htons(atoi(argv[1])+1);
 
-	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-
-	serv_addr.sin_port = htons(atoi(argv[1]));
-
-    if(bind(sock,(struct sockaddr*)&serv_addr,sizeof(serv_addr))==-1){
-        printf("bind error\n"); 
+    if(bind(sock1,(struct sockaddr*)&serv_addr1,sizeof(serv_addr1))==-1){
+        printf("bind 1error\n"); 
+        exit(1); }
+    if(bind(sock2,(struct sockaddr*)&serv_addr2,sizeof(serv_addr2))==-1){
+        printf("bind 2error\n"); 
         exit(1); }
        
-        listen(sock,5);
-for(int i=0;i<5;i++){
-        clnt_sock = accept(sock,(struct sockaddr*)&clnt_addr,&clnt_addr_size);
+        listen(sock1,5);
+        listen(sock2,5);
 
-	write(clnt_sock, &s_message[i], sizeof(s_message[i]));
+        if((clnt_sock1 = accept(sock1,(struct sockaddr*)&clnt_addr,&clnt_addr_size))<0)
+            error_handling("accept error");
+        if((clnt_sock2 = accept(sock2,(struct sockaddr*)&clnt_addr,&clnt_addr_size))<0)
+            error_handling("accept error");
+        clientsock[0] = clnt_sock1;
+	clientsock[1] = clnt_sock2;
+	pthread_create(&tid[0],NULL,&thr_func,(void *)&clnt_sock1);
+        pthread_create(&tid[1],NULL,&thr_func,(void *)&clnt_sock2);
+	
+	pthread_join(tid[0],(void **)&status);
+	pthread_join(tid[1],(void **)&status);
+
+	return 0 ; 
+}
+void *thr_func(void *arg){
+int mysock = (int)*((int *)arg);
+int hissock;
+if( mysock == clientsock[0] ) 
+	hissock = clientsock[1];
+else 
+	hissock = clientsock[0];
+while(1){
+if(read(mysock,&buffer,sizeof(buffer)!=0)){
+	printf(" Read from clnt_sock[%d] buffer : %c\n", mysock,buffer);
+	if(buffer == '0'){
+        write(hissock,&buffer,sizeof(buffer));
+	printf(" Well transported ! \n");  }
+}
+}
+}
         
-        str_len = read(clnt_sock,message,sizeof(message)-1);
-        if(str_len == -1) 
-                error_handling("read() error");
-	printf("Message from client:%s\n",message);
-
-	close(clnt_sock);
-}
-	return 0;
-
-}
 
 
 
